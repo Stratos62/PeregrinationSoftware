564d1c3c97e96a163fcbfd58683e705238bc50d2
MoutainDewLord <andy.desmons@gmail.com>
2015-11-12 17:07:40 +0100

    Ajout variable a dans main clss

--- PeregrinationSoftware/src/mainPackage/Main.java
+++ PeregrinationSoftware/src/mainPackage/Main.java
@@ -16,5 +16,6 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
     	System.out.println("Hello World");
+        int a = 0;
     }
 }
